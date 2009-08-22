ed8842c8a0bbb2e120cdf198c242b210d7fb342e
Alex Aranda <4200310+fleax@users.noreply.github.com>
2009-08-22 11:21:33 +0000

    FIX: Images with alpha channel are displayed wrong

--- aTunes/src/main/java/net/sourceforge/atunes/utils/ImageUtils.java
+++ aTunes/src/main/java/net/sourceforge/atunes/utils/ImageUtils.java
@@ -106,7 +106,7 @@ public final class ImageUtils {
         }
 
         if (imageWidth <= calculatedWidth && imageHeight <= calculatedHeight) {
-            BufferedImage thumbImage = new BufferedImage(calculatedWidth, calculatedHeight, BufferedImage.TYPE_INT_RGB);
+            BufferedImage thumbImage = new BufferedImage(calculatedWidth, calculatedHeight, BufferedImage.TYPE_INT_ARGB);
             Graphics2D graphics2D = thumbImage.createGraphics();
             graphics2D.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
             graphics2D.drawImage(image, 0, 0, calculatedWidth, calculatedHeight, null);
@@ -148,7 +148,7 @@ public final class ImageUtils {
             calculatedWidth = (int) (height * imageRatio);
         }
 
-        BufferedImage thumbImage = new BufferedImage(calculatedWidth, calculatedHeight, BufferedImage.TYPE_INT_RGB);
+        BufferedImage thumbImage = new BufferedImage(calculatedWidth, calculatedHeight, BufferedImage.TYPE_INT_ARGB);
         Graphics2D graphics2D = thumbImage.createGraphics();
         graphics2D.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
         graphics2D.drawImage(image, 0, 0, calculatedWidth, calculatedHeight, null);
