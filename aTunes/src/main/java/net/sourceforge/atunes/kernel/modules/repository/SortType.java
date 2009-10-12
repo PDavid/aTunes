019497989d1159f28c5485c27390bd8f14207fee
Alex Aranda <4200310+fleax@users.noreply.github.com>
2009-10-12 17:39:39 +0000

    Moved SortType to its own file / moved calls to sort

--- aTunes/src/main/java/net/sourceforge/atunes/kernel/modules/repository/SortType.java
+++ aTunes/src/main/java/net/sourceforge/atunes/kernel/modules/repository/SortType.java
@@ -11,97 +11,20 @@ import net.sourceforge.atunes.model.AudioObject;
 public enum SortType {
 
 	/** by track sort order */
-	BY_TRACK_NUMBER(new Comparator<AudioObject>() {
-		@Override
-		public int compare(AudioObject a1, AudioObject a2) {
-			return Integer.valueOf(a1.getTrackNumber()).compareTo(a2.getTrackNumber());
-		}
-	}),
+	BY_TRACK_NUMBER,
 
 	/** by artist sort order */
-	BY_ARTIST_AND_ALBUM(new Comparator<AudioObject>() {
-		@Override
-		public int compare(AudioObject a1, AudioObject a2) {
-
-			// Sort by album artist
-			int c1 = a1.getAlbumArtist().compareTo(a2.getAlbumArtist());
-			if (c1 != 0) {
-				return c1;
-			}
-
-			/*
-			 * If album artist is "" in both audio objects (we just need
-			 * to check only one audio object since if execution reaches
-			 * this code both album artist fields are equal) then sort
-			 * by artist, album and track If album artist is not "",
-			 * then only sort by album and track
-			 */
-			if (a1.getAlbumArtist().isEmpty()) {
-				int c2 = a1.getArtist().compareTo(a2.getArtist());
-				if (c2 != 0) {
-					return c2;
-				}
-			}
-
-			// Sort by album
-			int c3 = a1.getAlbum().compareTo(a2.getAlbum());
-			if (c3 != 0) {
-				return c3;
-			}
-
-			// Sort by disc number
-			int c4 = Integer.valueOf(a1.getDiscNumber()).compareTo(a2.getDiscNumber());
-			if (c4 != 0) {
-				return c4;
-			}
-
-			return Integer.valueOf(a1.getTrackNumber()).compareTo(a2.getTrackNumber());
-		}
-	}),
+	BY_ARTIST_AND_ALBUM,
 
 	/** by title sort order */
-	BY_TITLE(new Comparator<AudioObject>() {
-		@Override
-		public int compare(AudioObject a0, AudioObject a1) {
-			return a0.getTitleOrFileName().compareTo(a1.getTitleOrFileName());
-		}
-	}),
+	BY_TITLE,
 
 	/** by file name sort order */
-	BY_FILE(new Comparator<AudioObject>() {
-		@Override
-		public int compare(AudioObject o1, AudioObject o2) {
-			if (o1 instanceof AudioFile && o2 instanceof AudioFile) {
-				return ((AudioFile) o1).getFile().getName().compareTo(((AudioFile) o2).getFile().getName());
-			}
-			return 0;
-		}
-	}),
+	BY_FILE,
 
     /** by modification date sort order */
-    BY_MODIFICATION_TIME(new Comparator<AudioObject>() {
-    	@Override
-    	public int compare(AudioObject o1, AudioObject o2) {
-    		if (o1 instanceof AudioFile && o2 instanceof AudioFile) {
-    			return Long.valueOf(((AudioFile) o1).getFile().lastModified()).compareTo(Long.valueOf(((AudioFile) o2).getFile().lastModified()));
-    		}
-    		return 0;
-    	}
-    });
+    BY_MODIFICATION_TIME;
     
-	/**
-	 * Comparator used for the SortType instance
-	 */
-    private Comparator<AudioObject> sorter;
-    
-    /**
-     * Constructor for every sort type, defining comparator used in sort operations
-     * @param sorter
-     */
-    private SortType(Comparator<AudioObject> sorter) {
-    	this.sorter = sorter;
-    }
-
     /**
      * Sort.
      * 
@@ -126,8 +49,88 @@ public enum SortType {
      */
     public static List<AudioObject> sort(List<? extends AudioObject> audioObjects, SortType type) {
         AudioObject[] array = audioObjects.toArray(new AudioObject[audioObjects.size()]);
-        Arrays.sort(array, type.sorter);
+        Arrays.sort(array, getComparator(type));
         return Arrays.asList(array);
+    }
+    
+    /**
+     * Returns appropiate Comparator for SortType
+     * @param type
+     * @return
+     */
+    private static Comparator<AudioObject> getComparator(SortType type) {
+    	switch (type) {
+    	case BY_TRACK_NUMBER: return new Comparator<AudioObject>() {
+    		@Override
+    		public int compare(AudioObject a1, AudioObject a2) {
+    			return Integer.valueOf(a1.getTrackNumber()).compareTo(a2.getTrackNumber());
+    		}
+    	};
+    	case BY_ARTIST_AND_ALBUM: return new Comparator<AudioObject>() {
+    		@Override
+    		public int compare(AudioObject a1, AudioObject a2) {
+
+    			// Sort by album artist
+    			int c1 = a1.getAlbumArtist().compareTo(a2.getAlbumArtist());
+    			if (c1 != 0) {
+    				return c1;
+    			}
+
+    			/*
+    			 * If album artist is "" in both audio objects (we just need
+    			 * to check only one audio object since if execution reaches
+    			 * this code both album artist fields are equal) then sort
+    			 * by artist, album and track If album artist is not "",
+    			 * then only sort by album and track
+    			 */
+    			if (a1.getAlbumArtist().isEmpty()) {
+    				int c2 = a1.getArtist().compareTo(a2.getArtist());
+    				if (c2 != 0) {
+    					return c2;
+    				}
+    			}
+
+    			// Sort by album
+    			int c3 = a1.getAlbum().compareTo(a2.getAlbum());
+    			if (c3 != 0) {
+    				return c3;
+    			}
+
+    			// Sort by disc number
+    			int c4 = Integer.valueOf(a1.getDiscNumber()).compareTo(a2.getDiscNumber());
+    			if (c4 != 0) {
+    				return c4;
+    			}
+
+    			return Integer.valueOf(a1.getTrackNumber()).compareTo(a2.getTrackNumber());
+    		}
+    	};
+    	case BY_TITLE: return new Comparator<AudioObject>() {
+    		@Override
+    		public int compare(AudioObject a0, AudioObject a1) {
+    			return a0.getTitleOrFileName().compareTo(a1.getTitleOrFileName());
+    		}
+    	};
+    	case BY_MODIFICATION_TIME: return new Comparator<AudioObject>() {
+        	@Override
+        	public int compare(AudioObject o1, AudioObject o2) {
+        		if (o1 instanceof AudioFile && o2 instanceof AudioFile) {
+        			return Long.valueOf(((AudioFile) o1).getFile().lastModified()).compareTo(Long.valueOf(((AudioFile) o2).getFile().lastModified()));
+        		}
+        		return 0;
+        	}
+        };
+    	case BY_FILE: return new Comparator<AudioObject>() {
+    		@Override
+    		public int compare(AudioObject o1, AudioObject o2) {
+    			if (o1 instanceof AudioFile && o2 instanceof AudioFile) {
+    				return ((AudioFile) o1).getFile().getName().compareTo(((AudioFile) o2).getFile().getName());
+    			}
+    			return 0;
+    		}
+    	};
+    	default: return null;
+    	}
     }
 
 }
