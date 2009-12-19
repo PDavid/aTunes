207a187e03db997d560fd057e6084029b95b7080
Alex Aranda <4200310+fleax@users.noreply.github.com>
2009-12-19 10:36:21 +0000

    Added translation to Catalan (thanks to perru002)

--- aTunes/src/main/java/net/sourceforge/atunes/utils/I18nUtils.java
+++ aTunes/src/main/java/net/sourceforge/atunes/utils/I18nUtils.java
@@ -78,7 +78,7 @@ public final class I18nUtils {
      */
     public static List<Locale> getLanguages() {
         return Arrays
-                .asList(new Locale("ar"), new Locale("cs"), new Locale("da"), new Locale("de"), new Locale("el"), new Locale("en"), new Locale("es"), new Locale("fr"), new Locale(
+                .asList(new Locale("ar"), new Locale("ca"), new Locale("cs"), new Locale("da"), new Locale("de"), new Locale("el"), new Locale("en"), new Locale("es"), new Locale("fr"), new Locale(
                         "gl"), new Locale("hr"), new Locale("hu"), new Locale("it"), new Locale("ja"), new Locale("nl"), new Locale("no"), new Locale("pl"),
                         new Locale("pt", "BR"), new Locale("pt"), new Locale("ru"), new Locale("sk"), new Locale("sv"), new Locale("tr"), new Locale("uk"), new Locale("zh", "TW"),
                         new Locale("zh"));
