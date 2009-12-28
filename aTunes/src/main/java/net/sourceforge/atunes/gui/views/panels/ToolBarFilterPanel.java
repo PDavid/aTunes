a435f528a33bccb1e8ff3e6eb0c54c40eb64952a
tbeckers <tbeckers@cc6d23b5-4425-0410-adce-b9b1e8e37124>
2009-12-28 17:01:38 +0000

    updated german translation

--- aTunes/src/main/java/net/sourceforge/atunes/gui/views/panels/ToolBarFilterPanel.java
+++ aTunes/src/main/java/net/sourceforge/atunes/gui/views/panels/ToolBarFilterPanel.java
@@ -14,55 +14,49 @@ import net.sourceforge.atunes.utils.StringUtils;
 
 public class ToolBarFilterPanel extends JPanel {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1801321624657098000L;
-	
-	private PopUpButton filterButton;
-	
-	private JTextField filterTextField;
-	
-	private JButton clearFilterButton;
-	
-	public ToolBarFilterPanel() {
-		super(new GridBagLayout());
-		addContent();
-	}
-	
-	private void addContent() {
-		filterButton = new PopUpButton(StringUtils.getString(I18nUtils.getString("FILTER"), "..."), PopUpButton.BOTTOM_RIGHT);
-		GridBagConstraints c = new GridBagConstraints();
-		c.gridx = 0;
-		c.gridy = 0;
-		c.weightx = 1;
-		c.anchor = GridBagConstraints.EAST;
-		add(filterButton, c);
-		filterTextField = new JTextField(20);
-		filterTextField.setToolTipText(I18nUtils.getString("FILTER_TEXTFIELD_TOOLTIP"));
-		c.gridx = 1;
-		c.gridy = 0;
-		c.weightx = 0;
-		add(filterTextField, c);
-		clearFilterButton = new JButton(ImageLoader.getImage(ImageLoader.UNDO));
-		clearFilterButton.setToolTipText(I18nUtils.getString("CLEAR_FILTER_BUTTON_TOOLTIP"));
-		c.gridx = 2;
-		add(clearFilterButton, c);
-	}
+    private static final long serialVersionUID = 1801321624657098000L;
 
-	/**
-	 * @return the filterButton
-	 */
-	public PopUpButton getFilterButton() {
-		return filterButton;
-	}
+    private PopUpButton filterButton;
+    private JTextField filterTextField;
+    private JButton clearFilterButton;
 
-	/**
-	 * @return the filterTextField
-	 */
-	public JTextField getFilterTextField() {
-		return filterTextField;
-	}
-	
+    public ToolBarFilterPanel() {
+        super(new GridBagLayout());
+        addContent();
+    }
+
+    private void addContent() {
+        filterButton = new PopUpButton(StringUtils.getString(I18nUtils.getString("FILTER"), "..."), PopUpButton.BOTTOM_RIGHT);
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridx = 0;
+        c.gridy = 0;
+        c.weightx = 1;
+        c.anchor = GridBagConstraints.EAST;
+        add(filterButton, c);
+        filterTextField = new JTextField(20);
+        filterTextField.setToolTipText(I18nUtils.getString("FILTER_TEXTFIELD_TOOLTIP"));
+        c.gridx = 1;
+        c.gridy = 0;
+        c.weightx = 0;
+        add(filterTextField, c);
+        clearFilterButton = new JButton(ImageLoader.getImage(ImageLoader.UNDO));
+        clearFilterButton.setToolTipText(I18nUtils.getString("CLEAR_FILTER_BUTTON_TOOLTIP"));
+        c.gridx = 2;
+        add(clearFilterButton, c);
+    }
+
+    /**
+     * @return the filterButton
+     */
+    public PopUpButton getFilterButton() {
+        return filterButton;
+    }
+
+    /**
+     * @return the filterTextField
+     */
+    public JTextField getFilterTextField() {
+        return filterTextField;
+    }
 
 }
