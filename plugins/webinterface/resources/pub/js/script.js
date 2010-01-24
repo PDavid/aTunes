a807f4d850782c6288f60cc66b761a395a8cb771
Alex Aranda <4200310+fleax@users.noreply.github.com>
2010-01-24 11:19:18 +0000

    CSS for 320x480 resolution ready

--- plugins/webinterface/resources/pub/js/script.js
+++ plugins/webinterface/resources/pub/js/script.js
@@ -1,36 +1,131 @@
+var lastTitle = null;
+var lastArtist = null;
+
 function getActionsPackageName() {
 	return 'net.sourceforge.atunes.plugins.webinterface.actions';
 }
 
 function updateImage() {
-	var imageDiv = document.getElementById('current_player_image');
-	var randomnumber = Math.floor(Math.random()*10000)
-	var image = '<img class="current_image" src="/images?action='+getActionsPackageName()+'.CurrentImageAction&random='+randomnumber+'">';
-	imageDiv.innerHTML = image;
+	var randomnumber = Math.floor(Math.random()*10000)	
+	$('#current_image').attr('src', "/images?action="+getActionsPackageName()+".CurrentImageAction&random="+randomnumber);
 }
 
-function updateProperties() {
-	$("#current_ao_properties").load('/velocity?action='+getActionsPackageName()+'.GetCurrentAudioObjectAction&template=AudioObjectPropertiesTemplate.html');
+function callUpdateImage() {
+	/* See if audio object has changed */
+	var currentTitleDiv = document.getElementById('title');
+	var currentTitle;
+	if (currentTitleDiv != null) {
+		currentTitle = currentTitleDiv.firstChild.data;
+	}
+	var currentArtistDiv = document.getElementById('artist');
+	var currentArtist;
+	if (currentArtistDiv != null) {
+		currentArtist = currentArtistDiv.firstChild.data;
+	}
+	if (lastTitle == null || lastArtist == null || (lastTitle != currentTitle && lastArtist != currentArtist)) {
+		lastTitle = currentTitle;
+		lastArtist = currentArtist;
+		updateImage();		
+	}
+}
+
+function updateState() {
+	var state = $('#player_state').text();
+	
+	// Use state to set play or pause button
+	if (state == "PLAYING" || state == "RESUMING") {
+		$('#play').attr('src','images/pause.png');
+	} else {
+		$('#play').attr('src','images/play.png');
+	}
+	
+	// If stopped hide time and progressbar
+	if (state == "STOPPED") {
+		$('#elapsed_time').fadeOut('slow');
+		$('#remaining_time').fadeOut('slow');
+		$('#progressbar').fadeOut('slow');
+	} else {
+		$('#elapsed_time').fadeIn('slow');
+		$('#remaining_time').fadeIn('slow');
+		$('#progressbar').fadeIn('slow');
+	}
+}
+
+function updateControls() {
+	// Get player state
+	$('#player_state').load('/velocity?action='+getActionsPackageName()+'.PlayerControlsAction&template=PlayerControlsTemplate&option=state', updateState);
+}
+
+function millisecondsToString(milliseconds) {
+    var seconds = milliseconds / 1000;
+    var minutes = Math.floor(seconds / 60);
+    seconds = Math.round(seconds % 60);
+    return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+}
+
+function updateTimeControls() {
+	var elapsedTime = $('#elapsed_time_span').text();
+	var totalTime = $('#total_time_span').text();
+	var remainingTime = totalTime - elapsedTime;
+	
+	$('#elapsed_time').attr('innerHTML', '<span>'+millisecondsToString(elapsedTime)+'</span>');
+	$('#remaining_time').attr('innerHTML', '<span>-'+millisecondsToString(remainingTime)+'</span>');
+	
+	var progress = Math.round((elapsedTime / totalTime) * 100);
+	
+	$('#progressbar').progressbar('option', 'value', progress);
+}
+
+function updateTime() {
+	// Get player state
+	$('#time').load('/velocity?action='+getActionsPackageName()+'.PlayerControlsAction&template=TimeTemplate&option=time', updateTimeControls);
 }
 
 function update() {
-	updateImage();
-	updateProperties();
+
+	updateTime();
+	updateControls();
+	
+	// Update properties and image
+	$("#current_ao_properties").load('/velocity?action='+getActionsPackageName()+
+			'.GetCurrentAudioObjectAction&template=AudioObjectPropertiesTemplate.html', callUpdateImage);
+	
 }
 
 function initialize() {
+	var style320 = document.getElementById('style320_480');
+	var style480 = document.getElementById('style480_320');
+	var width = screen.width;
+	if (width < 480) {
+		style320.disabled = false;
+		style480.disabled = true;
+	} else {
+		style320.disabled = false;
+		style480.disabled = true;
+	}
+	
+	$("#progressbar").hide();
+	$("#progressbar").progressbar({
+		value: 0
+	});
+	$('#elapsed_time').hide();
+	$('#remaining_time').hide();
+
 	update();
 	
-	setInterval('update()',10000);
+	setInterval('update()',5000);	
 }
 
 function callPlayerControl(option) {
 	$.get('/velocity?action='+getActionsPackageName()+'.PlayerControlsAction&template=VoidTemplate&option='+option);
-	
+	/* Adding a callback function to call update seems to not work in mobile device */
 	update();
 }
 
 function stop() {
+	$('#elapsed_time').fadeOut('slow');
+	$('#remaining_time').fadeOut('slow');
+	$('#progressbar').fadeOut('slow');
 	callPlayerControl('stop');
 }
 
@@ -44,4 +139,12 @@ function play() {
 
 function next() {
 	callPlayerControl('next');
+}
+
+function volumeDown() {
+	$.get('/velocity?action='+getActionsPackageName()+'.PlayerControlsAction&template=VoidTemplate&option=volumedown');
+}
+
+function volumeUp() {
+	$.get('/velocity?action='+getActionsPackageName()+'.PlayerControlsAction&template=VoidTemplate&option=volumeup');
 }
