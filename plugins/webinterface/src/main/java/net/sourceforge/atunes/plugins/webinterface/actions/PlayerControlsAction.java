45312a748f2fdb1768c4c8c7337087186b957f7e
Alex Aranda <4200310+fleax@users.noreply.github.com>
2010-01-24 11:12:11 +0000

    CSS for 320x480 resolution ready

--- plugins/webinterface/src/main/java/net/sourceforge/atunes/plugins/webinterface/actions/PlayerControlsAction.java
+++ plugins/webinterface/src/main/java/net/sourceforge/atunes/plugins/webinterface/actions/PlayerControlsAction.java
@@ -2,10 +2,11 @@ package net.sourceforge.atunes.plugins.webinterface.actions;
 
 import java.util.Map;
 
-import org.apache.velocity.VelocityContext;
-
 import net.sourceforge.atunes.api.PlayerApi;
 import net.sourceforge.atunes.plugins.webinterface.VelocityAction;
+import net.sourceforge.atunes.utils.StringUtils;
+
+import org.apache.velocity.VelocityContext;
 
 public class PlayerControlsAction extends VelocityAction {
 	
@@ -21,6 +22,20 @@ public class PlayerControlsAction extends VelocityAction {
 				PlayerApi.play();
 			} else if ("next".equals(option)) {
 				PlayerApi.next();
+			} else if ("volumedown".equals(option)) {
+				PlayerApi.volumeDown();
+			} else if ("volumeup".equals(option)) {
+				PlayerApi.volumeUp();
+			} else if ("state".equals(option)) {
+				VelocityContext context = new VelocityContext();
+				context.put("state", PlayerApi.getCurrentPlaybackState());
+				context.put("time", StringUtils.milliseconds2String(PlayerApi.getCurrentAudioObjectPlayedTime()));
+				return context;
+			} else if ("time".equals(option)) {
+				VelocityContext context = new VelocityContext();
+				context.put("elapsed_time", PlayerApi.getCurrentAudioObjectPlayedTime());
+				context.put("total_time", PlayerApi.getCurrentAudioObjectLength());
+				return context;
 			}
 		}
 		return null;
