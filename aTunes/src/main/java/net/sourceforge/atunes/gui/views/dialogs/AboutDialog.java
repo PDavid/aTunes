5e475a3ec8e866c768ae746956ab0587dad57d0d
tbeckers <tbeckers@cc6d23b5-4425-0410-adce-b9b1e8e37124>
2010-03-14 10:01:05 +0000

    updated year

--- aTunes/src/main/java/net/sourceforge/atunes/gui/views/dialogs/AboutDialog.java
+++ aTunes/src/main/java/net/sourceforge/atunes/gui/views/dialogs/AboutDialog.java
@@ -270,7 +270,7 @@ public final class AboutDialog extends CustomModalDialog {
      * @return the license text
      */
     private String getLicenseText() {
-        return StringUtils.getString("Copyright (C) 2006-2009  The aTunes Team\n\n", "This program is free software; you can redistribute it and/or ",
+        return StringUtils.getString("Copyright (C) 2006-2010  The aTunes Team\n\n", "This program is free software; you can redistribute it and/or ",
                 "modify it under the terms of the GNU General Public License ", "as published by the Free Software Foundation; either version 2 ",
                 "of the License, or (at your option) any later version.\n\n", "This program is distributed in the hope that it will be useful, ",
                 "but WITHOUT ANY WARRANTY; without even the implied warranty of ", "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the ",
