804d4552b2082afcedb5d2f56e0c5f85141c12fb
Alex Aranda <4200310+fleax@users.noreply.github.com>
2011-01-07 08:42:57 +0000

    Center all modal dialogs in screen

--- aTunes/src/main/java/net/sourceforge/atunes/gui/views/controls/AbstractCustomModalDialog.java
+++ aTunes/src/main/java/net/sourceforge/atunes/gui/views/controls/AbstractCustomModalDialog.java
@@ -51,7 +51,7 @@ public abstract class AbstractCustomModalDialog extends JDialog {
         setSize(width, height);
         setUndecorated(LookAndFeelSelector.getInstance().getCurrentLookAndFeel().isDialogUndecorated());
         setModalityType(modal ? ModalityType.APPLICATION_MODAL : ModalityType.MODELESS);
-        setLocationRelativeTo(owner);
+        setLocationRelativeTo(null); // Center all modal dialogs in screen
         setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
     }
 
