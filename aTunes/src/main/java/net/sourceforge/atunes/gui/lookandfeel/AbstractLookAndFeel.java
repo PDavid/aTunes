abac44418a23ac1b72a58a7fa6c120f76e6322dd
Alex Aranda <4200310+fleax@users.noreply.github.com>
2011-05-29 16:25:01 +0000

    Look and feel can not support font settings

--- aTunes/src/main/java/net/sourceforge/atunes/gui/lookandfeel/AbstractLookAndFeel.java
+++ aTunes/src/main/java/net/sourceforge/atunes/gui/lookandfeel/AbstractLookAndFeel.java
@@ -41,9 +41,6 @@ import javax.swing.table.TableCellRenderer;
 import javax.swing.tree.DefaultTreeCellRenderer;
 import javax.swing.tree.TreeCellRenderer;
 
-import net.sourceforge.atunes.misc.log.LogCategories;
-import net.sourceforge.atunes.misc.log.Logger;
-
 import org.commonjukebox.plugins.model.PluginApi;
 
 @PluginApi
@@ -266,7 +263,7 @@ public abstract class AbstractLookAndFeel {
     	while (keys.hasMoreElements()) {
     		Object key = keys.nextElement();
     		Object value = UIManager.get (key);
-    		Logger.debug(LogCategories.DESKTOP, key, " = ", value);
+//    		Logger.debug(LogCategories.DESKTOP, key, " = ", value);
     		if (value instanceof FontUIResource) {
     			UIManager.put (key, f);
     		}
