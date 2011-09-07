c0adb3be5dd1a7a0f8cd87dbb471b72785cffe39
Alex Aranda <4200310+fleax@users.noreply.github.com>
2011-09-07 18:49:44 +0000

    Create a custom DMG icon for Mac OS

---
+++ aTunes/build_tools/macos_createdmg.sh
@@ -0,0 +1,22 @@
+echo "SetFile in Volume icon"
+SetFile -c icnC $1/.VolumeIcon.icns
+echo "Creating DMG"
+hdiutil create -srcfolder $1 -volname "$2" -format UDRW -ov $3
+
+echo "Mount dmg and set icon"
+export DMG="$1/DMG"
+mkdir $DMG
+hdiutil attach $3 -mountpoint $DMG
+SetFile -a C $DMG
+
+echo "Umount dmg"
+hdiutil detach $DMG
+
+echo "convert to compressed"
+hdiutil convert $3 -format UDZO -o $4
+
+echo "Clean"
+rm -rf $3
+rm -rf $DMG
+
+echo "Finished"
