1190ed3c08445df00f3f76a1ebfe645b64962b3c
Alex Aranda <4200310+fleax@users.noreply.github.com>
2012-10-07 15:45:27 +0000

    Refactoring

--- aTunes/src/main/java/net/sourceforge/atunes/kernel/modules/navigator/NavigationHandler.java
+++ aTunes/src/main/java/net/sourceforge/atunes/kernel/modules/navigator/NavigationHandler.java
@@ -29,7 +29,6 @@ import java.util.Map;
 import javax.swing.JTree;
 import javax.swing.tree.DefaultMutableTreeNode;
 
-import net.sourceforge.atunes.Context;
 import net.sourceforge.atunes.kernel.AbstractHandler;
 import net.sourceforge.atunes.kernel.modules.draganddrop.TreeNavigationTransferHandler;
 import net.sourceforge.atunes.model.IAudioObject;
@@ -50,155 +49,166 @@ import org.commonjukebox.plugins.model.Plugin;
 import org.commonjukebox.plugins.model.PluginInfo;
 import org.commonjukebox.plugins.model.PluginListener;
 
+/**
+ * Responsible of manage navigator
+ * @author alex
+ *
+ */
 public final class NavigationHandler extends AbstractHandler implements PluginListener, INavigationHandler {
 
-    private List<INavigationView> navigationViews;
+	private List<INavigationView> navigationViews;
 
 	private NavigationController navigationController;
-	
+
 	private IFilterHandler filterHandler;
-	
+
 	private ITable navigationTable;
-	
+
 	private IFilter navigationTreeFilter;
-	
+
 	private IStateNavigation stateNavigation;
-	
+
+	/**
+	 * @param filterHandler
+	 */
+	public void setFilterHandler(final IFilterHandler filterHandler) {
+		this.filterHandler = filterHandler;
+	}
+
 	/**
 	 * @param stateNavigation
 	 */
-	public void setStateNavigation(IStateNavigation stateNavigation) {
+	public void setStateNavigation(final IStateNavigation stateNavigation) {
 		this.stateNavigation = stateNavigation;
 	}
-	
+
 	/**
 	 * @param navigationTreeFilter
 	 */
-	public void setNavigationTreeFilter(IFilter navigationTreeFilter) {
+	public void setNavigationTreeFilter(final IFilter navigationTreeFilter) {
 		this.navigationTreeFilter = navigationTreeFilter;
 	}
 
 	/**
 	 * @param navigationTable
 	 */
-	public void setNavigationTable(ITable navigationTable) {
+	public void setNavigationTable(final ITable navigationTable) {
 		this.navigationTable = navigationTable;
 	}
 
 	@Override
-	protected void initHandler() {
-		this.filterHandler = Context.getBean(IFilterHandler.class);
+	public void applicationStarted() {
+		showNavigationTree(stateNavigation.isShowNavigationTree());
+		applyNavigationTableVisibility(stateNavigation.isShowNavigationTree() && stateNavigation.isShowNavigationTable());
+
+		// Navigation Panel View
+		getNavigationController().setNavigationView(stateNavigation.getNavigationView(), false);
+
+		getNavigationController().getNavigationTreePanel().enableDragAndDrop(new TreeNavigationTransferHandler());
 	}
-	
-    @Override
-    public void applicationStarted() {
-        showNavigationTree(stateNavigation.isShowNavigationTree());
-        applyNavigationTableVisibility(stateNavigation.isShowNavigationTree() && stateNavigation.isShowNavigationTable());
-
-        // Navigation Panel View
-        getNavigationController().setNavigationView(stateNavigation.getNavigationView(), false); 
-        
-        getNavigationController().getNavigationTreePanel().enableDragAndDrop(new TreeNavigationTransferHandler());
-    }
-
-    @Override
-	public List<INavigationView> getNavigationViews() {
-        return navigationViews;
-    }
-
-    /**
-     * Builds a map containing classes of navigation view as keys and references
-     * to instances as values
-     * 
-     * @return
-     */
-    private Map<Class<? extends INavigationView>, INavigationView> getNavigationViewsMap() {
-        Map<Class<? extends INavigationView>, INavigationView> navigationViewsMap = new HashMap<Class<? extends INavigationView>, INavigationView>();
-        for (INavigationView view : getNavigationViews()) {
-            navigationViewsMap.put(view.getClass(), view);
-        }
-        return navigationViewsMap;
-    }
-
-    @Override
-	public INavigationView getCurrentView() {
-        return getView(getViewByName(stateNavigation.getNavigationView()));
-    }
-
-    @Override
-	public ViewMode getCurrentViewMode() {
-        return getCurrentView().getCurrentViewMode();
-    }
-
-    @Override
-	public INavigationView getView(Class<? extends INavigationView> navigationViewClass) {
-        return getNavigationViewsMap().get(navigationViewClass);
-    }
-    
-    @Override
-	public void refreshCurrentView() {
-        getCurrentView().refreshView(stateNavigation.getViewMode(),
-                filterHandler.getFilterText(navigationTreeFilter));
-    }
 
 	@Override
-	public void refreshView(INavigationView navigationView) {
-        if (navigationView.equals(getCurrentView())) {
-        	navigationView.refreshView(stateNavigation.getViewMode(),
-        			filterHandler.getFilterText(navigationTreeFilter));
-        }
+	public List<INavigationView> getNavigationViews() {
+		return navigationViews;
 	}
 
-    @Override
-	public Class<? extends INavigationView> getViewByName(String className) {
-        if (className == null) {
-            return null;
-        }
-        for (Class<? extends INavigationView> viewFromMap : getNavigationViewsMap().keySet()) {
-            if (viewFromMap.getName().equals(className)) {
-                return viewFromMap;
-            }
-        }
+	/**
+	 * Builds a map containing classes of navigation view as keys and references
+	 * to instances as values
+	 * 
+	 * @return
+	 */
+	private Map<Class<? extends INavigationView>, INavigationView> getNavigationViewsMap() {
+		Map<Class<? extends INavigationView>, INavigationView> navigationViewsMap = new HashMap<Class<? extends INavigationView>, INavigationView>();
+		for (INavigationView view : getNavigationViews()) {
+			navigationViewsMap.put(view.getClass(), view);
+		}
+		return navigationViewsMap;
+	}
 
-        // If class is not found (maybe the view was a plugin and has been removed, return default view)
-        return RepositoryNavigationView.class;
-    }
+	@Override
+	public INavigationView getCurrentView() {
+		return getView(getViewByName(stateNavigation.getNavigationView()));
+	}
 
-    @Override
-    public void pluginActivated(PluginInfo plugin) {
-        try {
-            getNavigationViews().add((INavigationView) getBean(IPluginsHandler.class).getNewInstance(plugin));
-            // Set tress
-            getNavigationController().getNavigationTreePanel().updateTrees();
-        } catch (PluginSystemException e) {
-            Logger.error(e);
-        }
-    }
+	@Override
+	public ViewMode getCurrentViewMode() {
+		return getCurrentView().getCurrentViewMode();
+	}
 
-    @Override
-    public void pluginDeactivated(PluginInfo plugin, Collection<Plugin> views) {
-        // Remove all views
-        for (Plugin view : views) {
-            getNavigationViews().remove(view);
-        }
-    }
+	@Override
+	public INavigationView getView(final Class<? extends INavigationView> navigationViewClass) {
+		return getNavigationViewsMap().get(navigationViewClass);
+	}
 
-    @Override
-    public void applicationStateChanged() {
-        refreshCurrentView();
-    }
+	@Override
+	public void refreshCurrentView() {
+		doRefreshView(getCurrentView());
+	}
 
-    /**
-     * Gets the navigation controller.
-     * 
-     * @return the navigation controller
-     */
-    private NavigationController getNavigationController() {
-    	if (navigationController == null) {
-    		navigationController = getBean(NavigationController.class);
-    	}
-        return navigationController;
-    }
+	@Override
+	public void refreshView(final INavigationView navigationView) {
+		if (navigationView.equals(getCurrentView())) {
+			doRefreshView(navigationView);
+		}
+	}
+
+	private void doRefreshView(final INavigationView navigationView) {
+		ViewMode viewMode = stateNavigation.getViewMode();
+		String filter = filterHandler.getFilterText(navigationTreeFilter);
+		navigationView.refreshView(viewMode, filter);
+	}
+
+	@Override
+	public Class<? extends INavigationView> getViewByName(final String className) {
+		if (className == null) {
+			return null;
+		}
+		for (Class<? extends INavigationView> viewFromMap : getNavigationViewsMap().keySet()) {
+			if (viewFromMap.getName().equals(className)) {
+				return viewFromMap;
+			}
+		}
+
+		// If class is not found (maybe the view was a plugin and has been removed, return default view)
+		return RepositoryNavigationView.class;
+	}
+
+	@Override
+	public void pluginActivated(final PluginInfo plugin) {
+		try {
+			getNavigationViews().add((INavigationView) getBean(IPluginsHandler.class).getNewInstance(plugin));
+			// Set tress
+			getNavigationController().getNavigationTreePanel().updateTrees();
+		} catch (PluginSystemException e) {
+			Logger.error(e);
+		}
+	}
+
+	@Override
+	public void pluginDeactivated(final PluginInfo plugin, final Collection<Plugin> views) {
+		// Remove all views
+		for (Plugin view : views) {
+			getNavigationViews().remove(view);
+		}
+	}
+
+	@Override
+	public void applicationStateChanged() {
+		refreshCurrentView();
+	}
+
+	/**
+	 * Gets the navigation controller.
+	 * 
+	 * @return the navigation controller
+	 */
+	private NavigationController getNavigationController() {
+		if (navigationController == null) {
+			navigationController = getBean(NavigationController.class);
+		}
+		return navigationController;
+	}
 
 	@Override
 	public void refreshNavigationTable() {
@@ -211,26 +221,26 @@ public final class NavigationHandler extends AbstractHandler implements PluginLi
 	}
 
 	@Override
-	public void setNavigationView(String name) {
-		getNavigationController().setNavigationView(name);		
+	public void setNavigationView(final String name) {
+		getNavigationController().setNavigationView(name);
 	}
 
 	@Override
-	public ISearch openSearchDialog(ISearchDialog dialog, boolean b) {
+	public ISearch openSearchDialog(final ISearchDialog dialog, final boolean b) {
 		return getNavigationController().openSearchDialog(dialog, b);
 	}
 
-	private void updateTableContent(JTree tree) {
+	private void updateTableContent(final JTree tree) {
 		getNavigationController().updateTableContent(tree);
 	}
-	
+
 	@Override
 	public void updateViewTable() {
 		updateTableContent(getCurrentView().getTree());
 	}
 
 	@Override
-	public List<? extends IAudioObject> getAudioObjectsForTreeNode(Class<? extends INavigationView> view, DefaultMutableTreeNode node) {
+	public List<? extends IAudioObject> getAudioObjectsForTreeNode(final Class<? extends INavigationView> view, final DefaultMutableTreeNode node) {
 		return getNavigationController().getAudioObjectsForTreeNode(view, node);
 	}
 
@@ -249,69 +259,69 @@ public final class NavigationHandler extends AbstractHandler implements PluginLi
 	}
 
 	@Override
-	public IAudioObject getAudioObjectInNavigationTable(int row) {
+	public IAudioObject getAudioObjectInNavigationTable(final int row) {
 		return getNavigationController().getAudioObjectInNavigationTable(row);
 	}
-	
+
 	@Override
 	public void repositoryReloaded() {
-    	// Calling inside invokeLater will cause ConcurrentModificationException
-    	refreshCurrentView();
+		// Calling inside invokeLater will cause ConcurrentModificationException
+		refreshCurrentView();
 	}
 
 	@Override
 	public void favoritesChanged() {
-        // Update all views
-        refreshCurrentView();
+		// Update all views
+		refreshCurrentView();
 	}
-	
-    @Override
-	public void showNavigationTree(boolean show) {    	
-        stateNavigation.setShowNavigationTree(show);
-
-    	// Disable or enable actions
-        for (INavigationView navigationView : getNavigationViews()) {
-        	navigationView.getActionToShowView().setEnabled(show);
-        }
-    	
-        getFrame().showNavigationTree(show);
-        
-        applyNavigationTableVisibility(show && stateNavigation.isShowNavigationTable());
-    }
-    
-    @Override
-	public void showNavigationTable(boolean show) {
-        stateNavigation.setShowNavigationTable(show);
-        applyNavigationTableVisibility(show);
-    }
-    
-    /**
-     * Used to show or hide navigation table when changing tree visibility
-     * @param show
-     */
-    private void applyNavigationTableVisibility(boolean show) {
-    	getFrame().showNavigationTable(show);
-        
-        updateTableContent(getCurrentView().getTree());
-    }
 
 	@Override
-	public void selectArtist(String artist) {
-		getCurrentView().selectArtist(getCurrentView().getCurrentViewMode(), artist);		
+	public void showNavigationTree(final boolean show) {
+		stateNavigation.setShowNavigationTree(show);
+
+		// Disable or enable actions
+		for (INavigationView navigationView : getNavigationViews()) {
+			navigationView.getActionToShowView().setEnabled(show);
+		}
+
+		getFrame().showNavigationTree(show);
+
+		applyNavigationTableVisibility(show && stateNavigation.isShowNavigationTable());
 	}
-	
-    @Override
-	public void selectAudioObject(IAudioObject audioObject){
-    	getCurrentView().selectAudioObject(getCurrentView().getCurrentViewMode(), audioObject);
-    }
+
+	@Override
+	public void showNavigationTable(final boolean show) {
+		stateNavigation.setShowNavigationTable(show);
+		applyNavigationTableVisibility(show);
+	}
+
+	/**
+	 * Used to show or hide navigation table when changing tree visibility
+	 * @param show
+	 */
+	private void applyNavigationTableVisibility(final boolean show) {
+		getFrame().showNavigationTable(show);
+
+		updateTableContent(getCurrentView().getTree());
+	}
+
+	@Override
+	public void selectArtist(final String artist) {
+		getCurrentView().selectArtist(getCurrentView().getCurrentViewMode(), artist);
+	}
+
+	@Override
+	public void selectAudioObject(final IAudioObject audioObject){
+		getCurrentView().selectAudioObject(getCurrentView().getCurrentViewMode(), audioObject);
+	}
 
 	@Override
 	public boolean isActionOverTree() {
 		return getNavigationController().isActionOverTree();
 	}
-	
+
 	@Override
-	public void setNavigationViews(List<INavigationView> navigationViews) {
+	public void setNavigationViews(final List<INavigationView> navigationViews) {
 		this.navigationViews = navigationViews;
-	}	
+	}
 }
