b9ccf7df59a234c32857ff7237b94bc614cf2b3e
Alex Aranda <4200310+fleax@users.noreply.github.com>
2012-11-17 14:15:28 +0000

    FIX: Avoid ConcurrentModificationException when reading tree objects while another thread is updating Repository

--- aTunes/src/main/java/net/sourceforge/atunes/kernel/modules/repository/Artist.java
+++ aTunes/src/main/java/net/sourceforge/atunes/kernel/modules/repository/Artist.java
@@ -21,9 +21,9 @@
 package net.sourceforge.atunes.kernel.modules.repository;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import net.sourceforge.atunes.model.IAlbum;
 import net.sourceforge.atunes.model.IArtist;
@@ -39,169 +39,172 @@ import org.commonjukebox.plugins.model.PluginApi;
 @PluginApi
 public class Artist implements IArtist {
 
-	/**
+    /**
 	 * 
 	 */
-	private static final long serialVersionUID = 2528917861599275619L;
+    private static final long serialVersionUID = 2528917861599275619L;
 
-	/** Name of the artist. */
-	String name;
+    /** Name of the artist. */
+    String name;
 
-	/** List of IAlbum objects, indexed by name. */
-	Map<String, IAlbum> albums;
+    /** List of IAlbum objects, indexed by name. */
+    Map<String, IAlbum> albums;
 
-	/**
-	 * Default constructor for serialization
-	 */
-	Artist() {
+    /**
+     * Default constructor for serialization
+     */
+    Artist() {
+    }
+
+    /**
+     * Constructor.
+     * 
+     * @param name
+     *            the name
+     */
+    public Artist(final String name) {
+	this.name = name;
+    }
+
+    /**
+     * Adds an album to this artist.
+     * 
+     * @param album
+     *            the album
+     */
+    @Override
+    public void addAlbum(final IAlbum album) {
+	getAlbums().put(album.getName(), album);
+    }
+
+    /**
+     * Comparator.
+     * 
+     * @param o
+     *            the o
+     * 
+     * @return the int
+     */
+    @Override
+    public int compareTo(final IArtist o) {
+	return this.name.compareToIgnoreCase(o.getName());
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+	if (!(o instanceof Artist)) {
+	    return false;
 	}
+	return ((Artist) o).name.equalsIgnoreCase(name);
+    }
 
-	/**
-	 * Constructor.
-	 * 
-	 * @param name
-	 *            the name
-	 */
-	public Artist(final String name) {
-		this.name = name;
+    /**
+     * Return an IAlbum for a given album name.
+     * 
+     * @param albumName
+     *            the album name
+     * 
+     * @return the album
+     */
+    @Override
+    public IAlbum getAlbum(final String albumName) {
+	return getAlbums().get(albumName);
+    }
+
+    /**
+     * Return albums of this artist.
+     * 
+     * @return the albums
+     */
+    @Override
+    public Map<String, IAlbum> getAlbums() {
+	if (albums == null) {
+	    // Use ConcurrentHashMap as read can happen while updating albums
+	    albums = new ConcurrentHashMap<String, IAlbum>();
 	}
+	return albums;
+    }
 
-	/**
-	 * Adds an album to this artist.
-	 * 
-	 * @param album
-	 *            the album
-	 */
-	@Override
-	public void addAlbum(final IAlbum album) {
-		getAlbums().put(album.getName(), album);
+    /**
+     * Returns a list of songs of this artist (all songs of all albums).
+     * 
+     * @return the audio objects
+     */
+    @Override
+    public List<ILocalAudioObject> getAudioObjects() {
+	List<ILocalAudioObject> songs = new ArrayList<ILocalAudioObject>();
+	for (IAlbum album : getAlbums().values()) {
+	    songs.addAll(album.getAudioObjects());
 	}
+	return songs;
+    }
 
-	/**
-	 * Comparator.
-	 * 
-	 * @param o
-	 *            the o
-	 * 
-	 * @return the int
-	 */
-	@Override
-	public int compareTo(final IArtist o) {
-		return this.name.compareToIgnoreCase(o.getName());
-	}
+    /**
+     * Returns the name of this artist.
+     * 
+     * @return the name
+     */
+    @Override
+    public String getName() {
+	return name;
+    }
 
-	@Override
-	public boolean equals(final Object o) {
-		if (!(o instanceof Artist)) {
-			return false;
+    @Override
+    public int hashCode() {
+	return name.hashCode();
+    }
+
+    /**
+     * Removes an album from this artist.
+     * 
+     * @param alb
+     *            the alb
+     */
+    @Override
+    public void removeAlbum(final IAlbum alb) {
+	getAlbums().remove(alb.getName());
+    }
+
+    /**
+     * String representation.
+     * 
+     * @return the string
+     */
+    @Override
+    public String toString() {
+	return getName();
+    }
+
+    /**
+     * Returns true if artist has no audio files
+     * 
+     * @return
+     */
+    @Override
+    public boolean isEmpty() {
+	if (!getAlbums().isEmpty()) {
+	    for (IAlbum a : getAlbums().values()) {
+		if (!a.isEmpty()) {
+		    return false;
 		}
-		return ((Artist) o).name.equalsIgnoreCase(name);
+	    }
 	}
+	return true;
+    }
 
-	/**
-	 * Return an IAlbum for a given album name.
-	 * 
-	 * @param albumName
-	 *            the album name
-	 * 
-	 * @return the album
-	 */
-	@Override
-	public IAlbum getAlbum(final String albumName) {
-		return getAlbums().get(albumName);
+    /**
+     * Returns number of audio files of artist
+     * 
+     * @return
+     */
+    @Override
+    public int size() {
+	int size = 0;
+	if (!getAlbums().isEmpty()) {
+	    for (IAlbum a : getAlbums().values()) {
+		size = size + a.size();
+	    }
 	}
-
-	/**
-	 * Return albums of this artist.
-	 * 
-	 * @return the albums
-	 */
-	@Override
-	public Map<String, IAlbum> getAlbums() {
-		if (albums == null) {
-			albums = new HashMap<String, IAlbum>();
-		}
-		return albums;
-	}
-
-	/**
-	 * Returns a list of songs of this artist (all songs of all albums).
-	 * 
-	 * @return the audio objects
-	 */
-	@Override
-	public List<ILocalAudioObject> getAudioObjects() {
-		List<ILocalAudioObject> songs = new ArrayList<ILocalAudioObject>();
-		for (IAlbum album : getAlbums().values()) {
-			songs.addAll(album.getAudioObjects());
-		}
-		return songs;
-	}
-
-	/**
-	 * Returns the name of this artist.
-	 * 
-	 * @return the name
-	 */
-	@Override
-	public String getName() {
-		return name;
-	}
-
-	@Override
-	public int hashCode() {
-		return name.hashCode();
-	}
-
-	/**
-	 * Removes an album from this artist.
-	 * 
-	 * @param alb
-	 *            the alb
-	 */
-	@Override
-	public void removeAlbum(final IAlbum alb) {
-		getAlbums().remove(alb.getName());
-	}
-
-	/**
-	 * String representation.
-	 * 
-	 * @return the string
-	 */
-	@Override
-	public String toString() {
-		return getName();
-	}
-
-	/**
-	 * Returns true if artist has no audio files
-	 * @return
-	 */
-	@Override
-	public boolean isEmpty() {
-		if (!getAlbums().isEmpty()) {
-			for (IAlbum a : getAlbums().values()) {
-				if (!a.isEmpty()) {
-					return false;
-				}
-			}
-		}
-		return true;
-	}
-
-	/**
-	 * Returns number of audio files of artist
-	 * @return
-	 */
-	@Override
-	public int size() {
-		int size = 0;
-		if (!getAlbums().isEmpty()) {
-			for (IAlbum a : getAlbums().values()) {
-				size = size + a.size();
-			}
-		}
-		return size;
-	}
+	return size;
+    }
 }
--- aTunes/src/main/java/net/sourceforge/atunes/kernel/modules/repository/Folder.java
+++ aTunes/src/main/java/net/sourceforge/atunes/kernel/modules/repository/Folder.java
@@ -22,9 +22,9 @@ package net.sourceforge.atunes.kernel.modules.repository;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import net.sourceforge.atunes.model.IFolder;
 import net.sourceforge.atunes.model.ILocalAudioObject;
@@ -39,253 +39,259 @@ import net.sourceforge.atunes.utils.StringUtils;
  */
 public class Folder implements IFolder {
 
-	/**
+    /**
 	 * 
 	 */
-	private static final long serialVersionUID = 7175829016580923961L;
+    private static final long serialVersionUID = 7175829016580923961L;
 
-	/** Name of the folder. */
-	String name;
+    /** Name of the folder. */
+    String name;
 
-	/** List of files in this folder. */
-	List<ILocalAudioObject> files;
+    /** List of files in this folder. */
+    List<ILocalAudioObject> files;
 
-	/** List of folders in this folder, indexed by name. */
-	Map<String, IFolder> folders;
+    /** List of folders in this folder, indexed by name. */
+    Map<String, IFolder> folders;
 
-	/** Folder that contains this folder. */
-	IFolder parentFolder;
+    /** Folder that contains this folder. */
+    IFolder parentFolder;
 
-	/**
-	 * Default constructor for serialization
-	 */
-	Folder() {
+    /**
+     * Default constructor for serialization
+     */
+    Folder() {
+    }
+
+    /**
+     * Constructor.
+     * 
+     * @param name
+     *            the name
+     */
+    public Folder(final String name) {
+	this.name = name;
+    }
+
+    /**
+     * Adds a file to this folder.
+     * 
+     * @param file
+     *            the file
+     */
+    @Override
+    public void addAudioFile(final ILocalAudioObject file) {
+	getFiles().add(file);
+    }
+
+    /**
+     * Adds a folder as child of this folder.
+     * 
+     * @param f
+     *            the f
+     */
+    @Override
+    public void addFolder(final IFolder f) {
+	if (getFolders().containsKey(f.getName())) {
+	    getFolders().get(f.getName()).getFolders().putAll(f.getFolders());
+	} else {
+	    getFolders().put(f.getName(), f);
+	    f.setParentFolder(this);
 	}
+    }
 
-	/**
-	 * Constructor.
-	 * 
-	 * @param name
-	 *            the name
-	 */
-	public Folder(final String name) {
-		this.name = name;
+    /**
+     * Returns a list of songs in this folder and in children folders.
+     * 
+     * @return the audio objects
+     */
+    @Override
+    public List<ILocalAudioObject> getAudioObjects() {
+	List<ILocalAudioObject> result = null;
+	for (IFolder f : getFolders().values()) {
+	    if (result == null) {
+		result = f.getAudioObjects();
+	    } else {
+		result.addAll(f.getAudioObjects());
+	    }
 	}
+	if (result == null) {
+	    result = new ArrayList<ILocalAudioObject>();
+	}
+	result.addAll(getFiles());
+	return result;
+    }
 
-	/**
-	 * Adds a file to this folder.
-	 * 
-	 * @param file
-	 *            the file
-	 */
-	@Override
-	public void addAudioFile(final ILocalAudioObject file) {
-		getFiles().add(file);
+    /**
+     * Returns files in this folder.
+     * 
+     * @return the files
+     */
+    private List<ILocalAudioObject> getFiles() {
+	if (files == null) {
+	    files = new ArrayList<ILocalAudioObject>();
 	}
+	return files;
+    }
 
-	/**
-	 * Adds a folder as child of this folder.
-	 * 
-	 * @param f
-	 *            the f
-	 */
-	@Override
-	public void addFolder(final IFolder f) {
-		if (getFolders().containsKey(f.getName())) {
-			getFolders().get(f.getName()).getFolders().putAll(f.getFolders());
-		} else {
-			getFolders().put(f.getName(), f);
-			f.setParentFolder(this);
-		}
-	}
+    /**
+     * Returns a child folder given a folder name.
+     * 
+     * @param folderName
+     *            the folder name
+     * 
+     * @return the folder
+     */
+    @Override
+    public IFolder getFolder(final String folderName) {
+	return getFolders().get(folderName);
+    }
 
-	/**
-	 * Returns a list of songs in this folder and in children folders.
-	 * 
-	 * @return the audio objects
-	 */
-	@Override
-	public List<ILocalAudioObject> getAudioObjects() {
-		List<ILocalAudioObject> result = null;
-		for (IFolder f : getFolders().values()) {
-			if (result == null) {
-				result = f.getAudioObjects();
-			} else {
-				result.addAll(f.getAudioObjects());
-			}
-		}
-		if (result == null) {
-			result = new ArrayList<ILocalAudioObject>();
-		}
-		result.addAll(getFiles());
-		return result;
+    /**
+     * Returns all children folders.
+     * 
+     * @return the folders
+     */
+    @Override
+    public Map<String, IFolder> getFolders() {
+	if (folders == null) {
+	    // Use ConcurrentHashMap to avoid ConcurrentModificationException
+	    // reading folders while another thread
+	    // is updating them
+	    folders = new ConcurrentHashMap<String, IFolder>();
 	}
+	return folders;
+    }
 
-	/**
-	 * Returns files in this folder.
-	 * 
-	 * @return the files
-	 */
-	private List<ILocalAudioObject> getFiles() {
-		if (files == null) {
-			files = new ArrayList<ILocalAudioObject>();
-		}
-		return files;
-	}
+    /**
+     * Returns the name of this folder.
+     * 
+     * @return the name
+     */
+    @Override
+    public String getName() {
+	return name;
+    }
 
-	/**
-	 * Returns a child folder given a folder name.
-	 * 
-	 * @param folderName
-	 *            the folder name
-	 * 
-	 * @return the folder
-	 */
-	@Override
-	public IFolder getFolder(final String folderName) {
-		return getFolders().get(folderName);
-	}
+    /**
+     * Gets the parent folder.
+     * 
+     * @return the parentFolder
+     */
+    @Override
+    public IFolder getParentFolder() {
+	return parentFolder;
+    }
 
-	/**
-	 * Returns all children folders.
-	 * 
-	 * @return the folders
-	 */
-	@Override
-	public Map<String, IFolder> getFolders() {
-		if (folders == null) {
-			folders = new HashMap<String, IFolder>();
-		}
-		return folders;
-	}
+    /**
+     * Returns true if folder is empty (contains neither files nor folders).
+     * 
+     * @return true, if checks if is empty
+     */
+    @Override
+    public boolean isEmpty() {
+	return getFiles().isEmpty() && getFolders().isEmpty();
+    }
 
-	/**
-	 * Returns the name of this folder.
-	 * 
-	 * @return the name
-	 */
-	@Override
-	public String getName() {
-		return name;
-	}
+    /**
+     * Removes a file from this folder
+     * 
+     * @param file
+     *            the file
+     */
+    @Override
+    public void removeAudioFile(final ILocalAudioObject file) {
+	getFiles().remove(file);
+    }
 
-	/**
-	 * Gets the parent folder.
-	 * 
-	 * @return the parentFolder
-	 */
-	@Override
-	public IFolder getParentFolder() {
-		return parentFolder;
-	}
+    /**
+     * Removes a folder from this folder.
+     * 
+     * @param f
+     *            the f
+     */
+    @Override
+    public void removeFolder(final IFolder f) {
+	getFolders().remove(f.getName());
+    }
 
-	/**
-	 * Returns true if folder is empty (contains neither files nor folders).
-	 * 
-	 * @return true, if checks if is empty
-	 */
-	@Override
-	public boolean isEmpty() {
-		return getFiles().isEmpty() && getFolders().isEmpty();
-	}
+    /**
+     * Sets the parent folder.
+     * 
+     * @param parentFolder
+     *            the parentFolder to set
+     */
+    @Override
+    public void setParentFolder(final IFolder parentFolder) {
+	this.parentFolder = parentFolder;
+    }
 
-	/**
-	 * Removes a file from this folder
-	 * 
-	 * @param file
-	 *            the file
-	 */
-	@Override
-	public void removeAudioFile(final ILocalAudioObject file) {
-		getFiles().remove(file);
-	}
+    /**
+     * String representation.
+     * 
+     * @return the string
+     */
+    @Override
+    public String toString() {
+	return name;
+    }
 
-	/**
-	 * Removes a folder from this folder.
-	 * 
-	 * @param f
-	 *            the f
-	 */
-	@Override
-	public void removeFolder(final IFolder f) {
-		getFolders().remove(f.getName());
+    @Override
+    public boolean equals(final Object o) {
+	if (!(o instanceof Folder)) {
+	    return false;
 	}
+	Folder folder = (Folder) o;
+	// Two folders are equal if have the same name and the same parent
+	if (folder.parentFolder != null && parentFolder != null) {
+	    return folder.name.equals(name)
+		    && folder.parentFolder.equals(parentFolder);
+	} else {
+	    if (folder.parentFolder == null && parentFolder == null) {
+		return folder.name.equals(name);
+	    } else {
+		return false;
+	    }
+	}
+    }
 
-	/**
-	 * Sets the parent folder.
-	 * 
-	 * @param parentFolder
-	 *            the parentFolder to set
-	 */
-	@Override
-	public void setParentFolder(final IFolder parentFolder) {
-		this.parentFolder = parentFolder;
+    @Override
+    public int hashCode() {
+	if (parentFolder == null) {
+	    return name.hashCode();
+	} else {
+	    return (name + parentFolder).hashCode();
 	}
+    }
 
-	/**
-	 * String representation.
-	 * 
-	 * @return the string
-	 */
-	@Override
-	public String toString() {
-		return name;
+    /**
+     * Returns the path of this folder
+     * 
+     * @param osManager
+     * @return
+     */
+    @Override
+    public File getFolderPath(final IOSManager osManager) {
+	String path = name;
+	IFolder parent = this.parentFolder;
+	while (parent != null) {
+	    path = StringUtils.getString(parent.getName(),
+		    osManager.getFileSeparator(), path);
+	    parent = parent.getParentFolder();
 	}
+	return new File(path);
+    }
 
-	@Override
-	public boolean equals(final Object o) {
-		if (!(o instanceof Folder)) {
-			return false;
-		}
-		Folder folder = (Folder) o;
-		// Two folders are equal if have the same name and the same parent
-		if (folder.parentFolder != null && parentFolder != null) {
-			return folder.name.equals(name) && folder.parentFolder.equals(parentFolder);
-		} else {
-			if (folder.parentFolder == null && parentFolder == null) {
-				return folder.name.equals(name);
-			} else {
-				return false;
-			}
-		}
+    /**
+     * Returns number of audio object
+     * 
+     * @return
+     */
+    @Override
+    public int size() {
+	int size = getFiles().size();
+	for (IFolder f : getFolders().values()) {
+	    size = size + f.size();
 	}
-
-	@Override
-	public int hashCode() {
-		if (parentFolder == null) {
-			return name.hashCode();
-		} else {
-			return (name + parentFolder).hashCode();
-		}
-	}
-
-	/**
-	 * Returns the path of this folder
-	 * 
-	 * @param osManager
-	 * @return
-	 */
-	@Override
-	public File getFolderPath(final IOSManager osManager) {
-		String path = name;
-		IFolder parent = this.parentFolder;
-		while (parent != null) {
-			path = StringUtils.getString(parent.getName(), osManager.getFileSeparator(), path);
-			parent = parent.getParentFolder();
-		}
-		return new File(path);
-	}
-
-	/**
-	 * Returns number of audio object
-	 * @return
-	 */
-	@Override
-	public int size() {
-		int size = getFiles().size();
-		for (IFolder f : getFolders().values()) {
-			size = size + f.size();
-		}
-		return size;
-	}
+	return size;
+    }
 }
