2b969d6549027ccd90992b9ad75a566d365dcf6d
Alex Aranda <4200310+fleax@users.noreply.github.com>
2012-12-01 16:47:56 +0000

    Sort similar artists by match value

--- aTunes/src/main/java/net/sourceforge/atunes/kernel/modules/webservices/lastfm/data/LastFmSimilarArtists.java
+++ aTunes/src/main/java/net/sourceforge/atunes/kernel/modules/webservices/lastfm/data/LastFmSimilarArtists.java
@@ -22,6 +22,7 @@ package net.sourceforge.atunes.kernel.modules.webservices.lastfm.data;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 import net.sourceforge.atunes.model.IArtistInfo;
@@ -37,133 +38,135 @@ import de.umass.lastfm.ImageSize;
  */
 public class LastFmSimilarArtists implements ISimilarArtistsInfo {
 
-    private static final long serialVersionUID = -8771547790339792068L;
+	private static final long serialVersionUID = -8771547790339792068L;
 
-    private String artistName;
-    private String picture;
-    private List<IArtistInfo> artists;
+	private String artistName;
+	private String picture;
+	private List<IArtistInfo> artists;
 
-    /**
-     * Gets the similar artists.
-     * 
-     * @param as
-     * @param a
-     * @param max
-     * @return
-     */
-    public static ISimilarArtistsInfo getSimilarArtists(
-	    final Collection<Artist> as, final Artist a, final int max) {
-	List<Artist> list = new ArrayList<Artist>(as);
-	LastFmSimilarArtists similar = new LastFmSimilarArtists();
+	/**
+	 * Gets the similar artists.
+	 * 
+	 * @param as
+	 * @param a
+	 * @param max
+	 * @return
+	 */
+	public static ISimilarArtistsInfo getSimilarArtists(
+			final Collection<Artist> as, final Artist a, final int max) {
+		List<Artist> list = new ArrayList<Artist>(as);
+		LastFmSimilarArtists similar = new LastFmSimilarArtists();
 
-	similar.artistName = a.getName();
-	similar.picture = a.getImageURL(ImageSize.LARGE);
+		similar.artistName = a.getName();
+		similar.picture = a.getImageURL(ImageSize.LARGE);
 
-	similar.artists = new ArrayList<IArtistInfo>();
-	for (int i = 0; i < list.size(); i++) {
-	    if (i == max) {
-		break;
-	    }
-	    similar.artists.add(LastFmArtist.getArtist(list.get(i),
-		    similar.artistName));
+		similar.artists = new ArrayList<IArtistInfo>();
+		for (int i = 0; i < list.size(); i++) {
+			if (i == max) {
+				break;
+			}
+			similar.artists.add(LastFmArtist.getArtist(list.get(i),
+					similar.artistName));
+		}
+
+		Collections.sort(similar.artists, new SimilarArtistComparator());
+
+		return similar;
 	}
 
-	return similar;
-    }
-
-    /**
-     * Gets the artist name.
-     * 
-     * @return the artist name
-     */
-    @Override
-    public String getArtistName() {
-	return artistName;
-    }
-
-    /**
-     * Gets the artists.
-     * 
-     * @return the artists
-     */
-    @Override
-    public List<IArtistInfo> getArtists() {
-	return artists;
-    }
-
-    /**
-     * Gets the picture.
-     * 
-     * @return the picture
-     */
-    @Override
-    public String getPicture() {
-	return picture;
-    }
-
-    /**
-     * Sets the artist name.
-     * 
-     * @param artistName
-     *            the artistName to set
-     */
-    @Override
-    public void setArtistName(final String artistName) {
-	this.artistName = artistName;
-    }
-
-    /**
-     * Sets the artists.
-     * 
-     * @param artists
-     *            the artists to set
-     */
-    @Override
-    public void setArtists(final List<? extends IArtistInfo> artists) {
-	this.artists = artists != null ? new ArrayList<IArtistInfo>(artists)
-		: null;
-    }
-
-    /**
-     * Sets the picture.
-     * 
-     * @param picture
-     *            the picture to set
-     */
-    @Override
-    public void setPicture(final String picture) {
-	this.picture = picture;
-    }
-
-    @Override
-    public int hashCode() {
-	final int prime = 31;
-	int result = 1;
-	result = prime * result
-		+ ((artistName == null) ? 0 : artistName.hashCode());
-	return result;
-    }
-
-    @Override
-    public boolean equals(final Object obj) {
-	if (this == obj) {
-	    return true;
+	/**
+	 * Gets the artist name.
+	 * 
+	 * @return the artist name
+	 */
+	@Override
+	public String getArtistName() {
+		return this.artistName;
 	}
-	if (obj == null) {
-	    return false;
+
+	/**
+	 * Gets the artists.
+	 * 
+	 * @return the artists
+	 */
+	@Override
+	public List<IArtistInfo> getArtists() {
+		return this.artists;
 	}
-	if (getClass() != obj.getClass()) {
-	    return false;
+
+	/**
+	 * Gets the picture.
+	 * 
+	 * @return the picture
+	 */
+	@Override
+	public String getPicture() {
+		return this.picture;
 	}
-	LastFmSimilarArtists other = (LastFmSimilarArtists) obj;
-	if (artistName == null) {
-	    if (other.artistName != null) {
-		return false;
-	    }
-	} else if (!artistName.equals(other.artistName)) {
-	    return false;
+
+	/**
+	 * Sets the artist name.
+	 * 
+	 * @param artistName
+	 *            the artistName to set
+	 */
+	@Override
+	public void setArtistName(final String artistName) {
+		this.artistName = artistName;
 	}
-	return true;
-    }
+
+	/**
+	 * Sets the artists.
+	 * 
+	 * @param artists
+	 *            the artists to set
+	 */
+	@Override
+	public void setArtists(final List<? extends IArtistInfo> artists) {
+		this.artists = artists != null ? new ArrayList<IArtistInfo>(artists)
+				: null;
+	}
+
+	/**
+	 * Sets the picture.
+	 * 
+	 * @param picture
+	 *            the picture to set
+	 */
+	@Override
+	public void setPicture(final String picture) {
+		this.picture = picture;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((this.artistName == null) ? 0 : this.artistName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		LastFmSimilarArtists other = (LastFmSimilarArtists) obj;
+		if (this.artistName == null) {
+			if (other.artistName != null) {
+				return false;
+			}
+		} else if (!this.artistName.equals(other.artistName)) {
+			return false;
+		}
+		return true;
+	}
 
 }
