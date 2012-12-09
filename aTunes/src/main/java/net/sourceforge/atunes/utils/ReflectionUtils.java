290ae804c5dac12bf23cfe370f7478bdc66a3a8c
Alex Aranda <4200310+fleax@users.noreply.github.com>
2012-12-09 14:10:53 +0000

    Add describe method using BeanUtils

--- aTunes/src/main/java/net/sourceforge/atunes/utils/ReflectionUtils.java
+++ aTunes/src/main/java/net/sourceforge/atunes/utils/ReflectionUtils.java
@@ -24,6 +24,9 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.util.Map;
+
+import org.apache.commons.beanutils.PropertyUtils;
 
 /**
  * Utilities for reflection
@@ -33,110 +36,130 @@ import java.lang.reflect.Type;
  */
 public final class ReflectionUtils {
 
-    private ReflectionUtils() {
-    }
-
-    /**
-     * Returns class or null if any exception happens
-     * 
-     * @param className
-     * @return
-     */
-    public static Class<?> getClass(final String className) {
-	try {
-	    return Class.forName(className);
-	} catch (ClassNotFoundException e) {
-	    Logger.error(e);
+	private ReflectionUtils() {
 	}
-	return null;
-    }
 
-    /**
-     * Creates a new instance of a class with a default constructor or returns
-     * null if any exception happens
-     * 
-     * @param className
-     * @return
-     */
-    public static Object newInstance(final String className) {
-	Class<?> clazz;
-	try {
-	    clazz = Class.forName(className);
-	    return clazz.getConstructor((Class[]) null).newInstance(
-		    (Object[]) null);
-	} catch (ClassNotFoundException e) {
-	    Logger.error(e);
-	} catch (IllegalArgumentException e) {
-	    Logger.error(e);
-	} catch (SecurityException e) {
-	    Logger.error(e);
-	} catch (InstantiationException e) {
-	    Logger.error(e);
-	} catch (IllegalAccessException e) {
-	    Logger.error(e);
-	} catch (InvocationTargetException e) {
-	    Logger.error(e);
-	} catch (NoSuchMethodException e) {
-	    Logger.error(e);
+	/**
+	 * Returns class or null if any exception happens
+	 * 
+	 * @param className
+	 * @return
+	 */
+	public static Class<?> getClass(final String className) {
+		try {
+			return Class.forName(className);
+		} catch (ClassNotFoundException e) {
+			Logger.error(e);
+		}
+		return null;
 	}
-	return null;
-    }
 
-    /**
-     * Returns method object or null if any error happens
-     * 
-     * @param clazz
-     * @param methodName
-     * @param arguments
-     * @return
-     */
-    public static Method getMethod(final Class<?> clazz,
-	    final String methodName, final Class<?>... arguments) {
-	try {
-	    return clazz.getDeclaredMethod(methodName, arguments);
-	} catch (IllegalArgumentException e) {
-	    Logger.error(e);
-	} catch (SecurityException e) {
-	    Logger.error(e);
-	} catch (NoSuchMethodException e) {
-	    Logger.error(e);
+	/**
+	 * Creates a new instance of a class with a default constructor or returns
+	 * null if any exception happens
+	 * 
+	 * @param className
+	 * @return
+	 */
+	public static Object newInstance(final String className) {
+		Class<?> clazz;
+		try {
+			clazz = Class.forName(className);
+			return clazz.getConstructor((Class[]) null).newInstance(
+					(Object[]) null);
+		} catch (ClassNotFoundException e) {
+			Logger.error(e);
+		} catch (IllegalArgumentException e) {
+			Logger.error(e);
+		} catch (SecurityException e) {
+			Logger.error(e);
+		} catch (InstantiationException e) {
+			Logger.error(e);
+		} catch (IllegalAccessException e) {
+			Logger.error(e);
+		} catch (InvocationTargetException e) {
+			Logger.error(e);
+		} catch (NoSuchMethodException e) {
+			Logger.error(e);
+		}
+		return null;
 	}
-	return null;
-    }
 
-    /**
-     * Invokes a method of an object with given arguments
-     * 
-     * @param instance
-     * @param method
-     * @param arguments
-     * @return result of invoke
-     */
-    public static Object invoke(final Object instance, final Method method,
-	    final Object... arguments) {
-	if (method != null) {
-	    try {
-		return method.invoke(instance, arguments);
-	    } catch (IllegalArgumentException e) {
-		Logger.error(e);
-	    } catch (IllegalAccessException e) {
-		Logger.error(e);
-	    } catch (InvocationTargetException e) {
-		Logger.error(e);
-	    }
+	/**
+	 * Returns method object or null if any error happens
+	 * 
+	 * @param clazz
+	 * @param methodName
+	 * @param arguments
+	 * @return
+	 */
+	public static Method getMethod(final Class<?> clazz,
+			final String methodName, final Class<?>... arguments) {
+		try {
+			return clazz.getDeclaredMethod(methodName, arguments);
+		} catch (IllegalArgumentException e) {
+			Logger.error(e);
+		} catch (SecurityException e) {
+			Logger.error(e);
+		} catch (NoSuchMethodException e) {
+			Logger.error(e);
+		}
+		return null;
 	}
-	return null;
-    }
 
-    /**
-     * Returns type arguments of a clazz
-     * 
-     * @param clazz
-     * @return
-     */
-    public static Type[] getTypeArgumentsOfParameterizedType(
-	    final Class<?> clazz) {
-	return ((ParameterizedType) clazz.getGenericSuperclass())
-		.getActualTypeArguments();
-    }
+	/**
+	 * Invokes a method of an object with given arguments
+	 * 
+	 * @param instance
+	 * @param method
+	 * @param arguments
+	 * @return result of invoke
+	 */
+	public static Object invoke(final Object instance, final Method method,
+			final Object... arguments) {
+		if (method != null) {
+			try {
+				return method.invoke(instance, arguments);
+			} catch (IllegalArgumentException e) {
+				Logger.error(e);
+			} catch (IllegalAccessException e) {
+				Logger.error(e);
+			} catch (InvocationTargetException e) {
+				Logger.error(e);
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Returns type arguments of a clazz
+	 * 
+	 * @param clazz
+	 * @return
+	 */
+	public static Type[] getTypeArgumentsOfParameterizedType(
+			final Class<?> clazz) {
+		return ((ParameterizedType) clazz.getGenericSuperclass())
+				.getActualTypeArguments();
+	}
+
+	/**
+	 * Returns map with properties and values of given object
+	 * 
+	 * @param bean
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public static Map<String, String> describe(final Object bean) {
+		try {
+			return PropertyUtils.describe(bean);
+		} catch (IllegalAccessException e) {
+			Logger.error(e);
+		} catch (InvocationTargetException e) {
+			Logger.error(e);
+		} catch (NoSuchMethodException e) {
+			Logger.error(e);
+		}
+		return null;
+	}
 }
