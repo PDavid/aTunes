a756b4d44533b9fa9dbe6d12e2c33e058e026f0f
Alex Aranda <4200310+fleax@users.noreply.github.com>
2012-12-20 15:26:25 +0000

    FIX: Updated heap sizes

--- aTunes/build_resources/aTunes.sh
+++ aTunes/build_resources/aTunes.sh
@@ -27,4 +27,4 @@ do
   args="$args \"$arg\""
 done
 
-java -Dinsubstantial.looseTableCellRenderers=true -Djava.library.path=./ -Xms64m -Xmx256m -splash:splash.gif -cp aTunes.jar:lib/* net.sourceforge.atunes.Main $args
+java -Dinsubstantial.looseTableCellRenderers=true -Djava.library.path=./ -Xms128m -Xmx256m -splash:splash.gif -cp aTunes.jar:lib/* net.sourceforge.atunes.Main $args
