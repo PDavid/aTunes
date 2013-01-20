005e7b9f7fe8660da772a1e3404609b1b32fba6e
Alex Aranda <4200310+fleax@users.noreply.github.com>
2013-01-20 13:35:17 +0000

    FIX: Seek in progress slider must be fired when releasing mouse button

--- aTunes/src/main/java/net/sourceforge/atunes/kernel/modules/player/ProgressBarSeekListener.java
+++ aTunes/src/main/java/net/sourceforge/atunes/kernel/modules/player/ProgressBarSeekListener.java
@@ -26,41 +26,45 @@ import java.awt.event.MouseEvent;
 import net.sourceforge.atunes.model.IPlayerHandler;
 import net.sourceforge.atunes.model.IProgressSlider;
 
-
 /**
  * Listener for seek operation in progress slider
+ * 
  * @author alex
- *
+ * 
  */
 public class ProgressBarSeekListener extends MouseAdapter {
 
-	private IProgressSlider progressBar;
-	
-	private IPlayerHandler playerHandler;
-	
+	private final IProgressSlider progressBar;
+
+	private final IPlayerHandler playerHandler;
+
 	/**
 	 * @param progressBar
 	 * @param playerHandler
 	 */
-	public ProgressBarSeekListener(IProgressSlider progressBar, IPlayerHandler playerHandler) {
+	public ProgressBarSeekListener(final IProgressSlider progressBar,
+			final IPlayerHandler playerHandler) {
 		super();
 		this.progressBar = progressBar;
 		this.playerHandler = playerHandler;
 	}
-	
-	@Override
-	public void mousePressed(MouseEvent e) {
-        if (progressBar.isEnabled()) {
 
-        	// Progress bar width is greater than real slider width so calculate value assuming 5 pixels in both left and right of track
-        	long temp = (long) progressBar.getMaximum() * (e.getX() - 5);
-        	int value = (int) (temp / (progressBar.getProgressBarWidth() - 10));
-        	
-        	float perCent = (((float) value) / playerHandler.getCurrentAudioObjectLength()) * 100.0f;
-        	
-        	// Force new value to avoid jump to next major tick
-        	progressBar.setValue(value);
-        	playerHandler.seekCurrentAudioObject(value, Math.round(perCent));
-        }
+	@Override
+	public void mouseReleased(final MouseEvent e) {
+		if (this.progressBar.isEnabled()) {
+
+			// Progress bar width is greater than real slider width so calculate
+			// value assuming 5 pixels in both left and right of track
+			long temp = (long) this.progressBar.getMaximum() * (e.getX() - 5);
+			int value = (int) (temp / (this.progressBar.getProgressBarWidth() - 10));
+
+			float perCent = (((float) value) / this.playerHandler
+					.getCurrentAudioObjectLength()) * 100.0f;
+
+			// Force new value to avoid jump to next major tick
+			this.progressBar.setValue(value);
+			this.playerHandler.seekCurrentAudioObject(value,
+					Math.round(perCent));
+		}
 	}
 }
