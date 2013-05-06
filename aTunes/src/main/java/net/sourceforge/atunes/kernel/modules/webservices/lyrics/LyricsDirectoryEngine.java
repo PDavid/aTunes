5e0a8791f68a060d6e13a9c20c73bf91aa4da16d
Alex Aranda <4200310+fleax@users.noreply.github.com>
2013-05-06 13:05:01 +0000

    FIX: LyricsDirectoryEngine review when no lyric found

--- aTunes/src/main/java/net/sourceforge/atunes/kernel/modules/webservices/lyrics/LyricsDirectoryEngine.java
+++ aTunes/src/main/java/net/sourceforge/atunes/kernel/modules/webservices/lyrics/LyricsDirectoryEngine.java
@@ -39,88 +39,90 @@ import net.sourceforge.atunes.utils.StringUtils;
  */
 public class LyricsDirectoryEngine extends AbstractLyricsEngine {
 
-    /**
-     * Text showing lyrics are incomplete
-     */
-    private static final String WE_ARE_CURRENTLY_UNABLE_TO_SHOW_YOU_THE_LYRICS = "We are currently unable to show you the lyrics";
-    private static final String BASE_URL = "http://www.lyricsdir.com/";
-    private static final String CHARSET = "UTF-8";
+	/**
+	 * Text showing lyrics are incomplete
+	 */
+	private static final String WE_ARE_CURRENTLY_UNABLE_TO_SHOW_YOU_THE_LYRICS = "We are currently unable to show you the lyrics";
+	private static final String BASE_URL = "http://www.lyricsdir.com/";
+	private static final String CHARSET = "UTF-8";
 
-    @Override
-    public ILyrics getLyricsFor(final String artist, final String title,
-	    final ILyricsRetrieveOperation operation) {
-	StringBuilder sb = new StringBuilder();
-	sb.append(BASE_URL);
-	sb.append(encodeString(getString(artist, title)));
-	sb.append("lyrics");
-	sb.append(".html");
+	@Override
+	public ILyrics getLyricsFor(final String artist, final String title,
+			final ILyricsRetrieveOperation operation) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(BASE_URL);
+		sb.append(encodeString(getString(artist, title)));
+		sb.append("lyrics");
+		sb.append(".html");
 
-	try {
-	    String html = readURL(getConnection(sb.toString(), operation),
-		    CHARSET);
+		try {
+			String html = readURL(getConnection(sb.toString(), operation),
+					CHARSET);
 
-	    String lyrics = null;
-	    Source source = new Source(html);
-	    List<Element> elements = source.getAllElements("div");
-	    for (Element element : elements) {
-		if ("protected_lyrics".equals(element.getAttributeValue("id"))) {
-		    lyrics = element.getRenderer().toString();
+			String lyrics = null;
+			Source source = new Source(html);
+			List<Element> elements = source.getAllElements("div");
+			for (Element element : elements) {
+				if ("protected_lyrics".equals(element.getAttributeValue("id"))) {
+					lyrics = element.getRenderer().toString();
+				}
+			}
+
+			lyrics = reviewLyrics(lyrics);
+
+			return lyrics != null && !lyrics.isEmpty() ? new Lyrics(lyrics,
+					sb.toString()) : null;
+		} catch (UnknownHostException e) {
+			Logger.error(StringUtils.getString(e.getClass().getCanonicalName(),
+					" (", e.getMessage(), ")"));
+			return null;
+		} catch (IOException e) {
+			Logger.error(StringUtils.getString(e.getClass().getCanonicalName(),
+					" (", e.getMessage(), ")"));
+			return null;
 		}
-	    }
-
-	    lyrics = reviewLyrics(lyrics);
-
-	    return lyrics != null && !lyrics.isEmpty() ? new Lyrics(lyrics,
-		    sb.toString()) : null;
-	} catch (UnknownHostException e) {
-	    Logger.error(StringUtils.getString(e.getClass().getCanonicalName(),
-		    " (", e.getMessage(), ")"));
-	    return null;
-	} catch (IOException e) {
-	    Logger.error(StringUtils.getString(e.getClass().getCanonicalName(),
-		    " (", e.getMessage(), ")"));
-	    return null;
 	}
-    }
 
-    /**
-     * Test if lyrics are partial. If so, discard them
-     * 
-     * @param lyrics
-     * @return
-     */
-    private String reviewLyrics(final String lyrics) {
-	if (lyrics.contains(WE_ARE_CURRENTLY_UNABLE_TO_SHOW_YOU_THE_LYRICS)) {
-	    return null;
+	/**
+	 * Test if lyrics are partial. If so, discard them
+	 * 
+	 * @param lyrics
+	 * @return
+	 */
+	private String reviewLyrics(final String lyrics) {
+		if (lyrics != null
+				&& lyrics
+						.contains(WE_ARE_CURRENTLY_UNABLE_TO_SHOW_YOU_THE_LYRICS)) {
+			return null;
+		}
+		return lyrics;
 	}
-	return lyrics;
-    }
 
-    private String getString(final String a, final String t) {
-	StringBuilder sb = new StringBuilder();
-	String artist = a.toLowerCase().trim();
-	artist = artist.replaceAll("\\W", " ");
-	String title = t.toLowerCase().trim();
-	title = title.replaceAll("\\W", " ");
-	for (String artistSplits : artist.split("\\s+")) {
-	    sb.append(artistSplits);
-	    sb.append("-");
+	private String getString(final String a, final String t) {
+		StringBuilder sb = new StringBuilder();
+		String artist = a.toLowerCase().trim();
+		artist = artist.replaceAll("\\W", " ");
+		String title = t.toLowerCase().trim();
+		title = title.replaceAll("\\W", " ");
+		for (String artistSplits : artist.split("\\s+")) {
+			sb.append(artistSplits);
+			sb.append("-");
+		}
+		for (String titleSplits : title.split(" ")) {
+			sb.append(titleSplits);
+			sb.append("-");
+		}
+		return sb.toString();
 	}
-	for (String titleSplits : title.split(" ")) {
-	    sb.append(titleSplits);
-	    sb.append("-");
+
+	@Override
+	public String getLyricsProviderName() {
+		return "LyricsDirectory";
 	}
-	return sb.toString();
-    }
 
-    @Override
-    public String getLyricsProviderName() {
-	return "LyricsDirectory";
-    }
-
-    @Override
-    public String getUrlForAddingNewLyrics(final String artist,
-	    final String title) {
-	return "";
-    }
+	@Override
+	public String getUrlForAddingNewLyrics(final String artist,
+			final String title) {
+		return "";
+	}
 }
