10b9f843bea152aca4ae0776f96778e28d4dc785
Alex Aranda <4200310+fleax@users.noreply.github.com>
2013-05-24 10:20:41 +0000

    FIX: overlay information not visible with low resolutions

--- aTunes/src/main/java/net/sourceforge/atunes/kernel/modules/navigator/AbstractNavigationView.java
+++ aTunes/src/main/java/net/sourceforge/atunes/kernel/modules/navigator/AbstractNavigationView.java
@@ -22,7 +22,6 @@ package net.sourceforge.atunes.kernel.modules.navigator;
 
 import java.awt.Color;
 import java.awt.Component;
-import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
@@ -102,15 +101,6 @@ public abstract class AbstractNavigationView implements INavigationView {
 	private IBeanFactory beanFactory;
 
 	private JPanel overlayPanel;
-
-	private Dimension navigationMinimumSize;
-
-	/**
-	 * @param navigationMinimumSize
-	 */
-	public void setNavigationMinimumSize(final Dimension navigationMinimumSize) {
-		this.navigationMinimumSize = navigationMinimumSize;
-	}
 
 	/**
 	 * @param beanFactory
@@ -208,28 +198,38 @@ public abstract class AbstractNavigationView implements INavigationView {
 	@Override
 	public final JPanel getOverlayPanel() {
 		if (this.overlayPanel == null) {
-			this.overlayPanel = new JPanel();
-			this.overlayPanel.setLayout(new GridBagLayout());
+			this.overlayPanel = new JPanel(new GridBagLayout());
+			JPanel panel = new JPanel();
+			panel.setBorder(BorderFactory.createLineBorder(GuiUtils
+					.getBorderColor()));
+			panel.setLayout(new GridBagLayout());
 			JTextPane text = getControlsBuilder().createReadOnlyTextPane(
 					getOverlayText());
+			JScrollPane scroll = getControlsBuilder().createScrollPane(text);
+			scroll.setBorder(BorderFactory.createEmptyBorder());
+			Action action = getOverlayAction();
 			GridBagConstraints c = new GridBagConstraints();
-			c.insets = new Insets(10, 10, 0, 10);
+			c.insets = new Insets(5, 5, 5, 5);
 			c.gridx = 0;
 			c.gridy = 0;
 			c.weightx = 1;
 			c.weighty = 1;
 			c.fill = GridBagConstraints.BOTH;
-			this.overlayPanel.add(text, c);
-			Action action = getOverlayAction();
+			panel.add(scroll, c);
 			if (action != null) {
 				c.gridy = 1;
 				c.weightx = 0;
 				c.weighty = 0;
 				c.fill = GridBagConstraints.NONE;
-				c.insets = new Insets(0, 10, 10, 10);
-				this.overlayPanel.add(new JButton(action), c);
+				c.insets = new Insets(0, 5, 5, 5);
+				panel.add(new JButton(action), c);
 			}
-			setupOverlayPanel(this.overlayPanel);
+			c = new GridBagConstraints();
+			c.weightx = 0.5;
+			c.weighty = 0.5;
+			c.insets = new Insets(20, 20, 20, 20);
+			c.fill = GridBagConstraints.BOTH;
+			this.overlayPanel.add(panel, c);
 		}
 		return this.overlayPanel;
 	}
@@ -279,11 +279,13 @@ public abstract class AbstractNavigationView implements INavigationView {
 	private void updateOverlayState() {
 		// Hide temporarily until adjusting visibility to avoid glitches of
 		// overlay layout
+		getTree().setVisible(false);
 		getOverlayPanel().setVisible(false);
 
 		boolean needsToBeVisible = overlayNeedsToBeVisible();
 		if (!getOverlayPanel().isVisible() && needsToBeVisible) {
 		}
+		getTree().setVisible(!needsToBeVisible);
 		getOverlayPanel().setVisible(needsToBeVisible);
 		getOverlayPanel().invalidate();
 		getOverlayPanel().repaint();
@@ -524,21 +526,6 @@ public abstract class AbstractNavigationView implements INavigationView {
 
 	@Override
 	public void selectArtist(final ViewMode currentViewMode, final String artist) {
-	}
-
-	private final JPanel setupOverlayPanel(final JPanel panel) {
-		// Limit overlay size to not be larger than navigator tree
-		Dimension size = new Dimension(
-				((int) this.navigationMinimumSize.getWidth()) - 75,
-				((int) this.navigationMinimumSize.getHeight()) - 75);
-		panel.setPreferredSize(size);
-		panel.setMaximumSize(size);
-		panel.setMinimumSize(size);
-
-		panel.setBorder(BorderFactory.createLineBorder(GuiUtils
-				.getBorderColor()));
-
-		return panel;
 	}
 
 	protected final IControlsBuilder getControlsBuilder() {
